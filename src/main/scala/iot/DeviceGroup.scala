240c60a42fb0dd5594d7ec0dc0340b459fd388d6
anb <anb@carecom.dk>
2017-07-12 15:11:48 +0200

    DeviceGroup Implementation
    TODO: Unit test

--- src/main/scala/iot/DeviceGroup.scala
+++ src/main/scala/iot/DeviceGroup.scala
@@ -1,7 +1,23 @@
 package iot
-import akka.actor.{Actor, ActorLogging, Props}
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
 class DeviceGroup private (val groupId: String) extends Actor with ActorLogging {
+  import DeviceManager._
+  private var deviceIdToActor = Map.empty[String, ActorRef]
+  override def preStart(): Unit = log.info(s"Device ${groupId} has been created")
+
+  override def receive: Receive = {
+    case trackMsg @ RequestTrackDevice(grId, _) if grId == groupId =>
+      deviceIdToActor.get(trackMsg.deviceId) match {
+        case Some(actor) => actor forward trackMsg
+        case None => {
+          val deviceActor = context.actorOf(Device.props(trackMsg.groupId, trackMsg.deviceId), s"device-${trackMsg.deviceId}")
+          deviceIdToActor += trackMsg.deviceId -> deviceActor
+        }
+      }
+    case msg @ RequestTrackDevice(_, _) => log.warning(s"Ignoring ${msg}")
+  }
 
 }
 
